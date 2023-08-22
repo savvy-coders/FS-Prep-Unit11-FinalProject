175c8b2e83854d78320e8fc665a081f07a34e573
David Rodriguez <davidrodrigueznolimit@gmail.com>
2023-08-22 14:17:27 -0500

    update tests to correct bugs

--- cypress/e2e/test31.cy.js
+++ cypress/e2e/test31.cy.js
@@ -1,5 +1,6 @@
 describe('should display date/time on click', () => {
     it('displays date/time', () => {
+      cy.visit('/');
       cy.get('#timeCheckBtn').click();
       cy.wait(5);
       cy.get('#time').should('not.be.empty');
--- script.js
+++ script.js
@@ -15,12 +15,3 @@ function getBtnAndShow(){
   setInterval(clearDateAndTime, 5000);
 
 
-
-
-// This export is to enable testing of your two testable primary functions.
-// PLEASE DO NOT EDIT below this line!!!
-
-module.exports.getBtnAndShow = getBtnAndShow;
-module.exports.clearDateAndTime = clearDateAndTime;
-
-
